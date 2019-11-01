4ee4d6db2d2e81265c86585706f32f17ef6f8e4e
lorrainepatricia <57242996+lorrainepatricia@users.noreply.github.com>
2019-11-01 09:37:21 +0700

    Add files via upload

---
+++ src/components/menu.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Link } from "gatsby"
+    
+const Menu = () => (
+    <div style={{ 
+        background: '#f4f4f4',
+        paddingTop: '10px'
+     }}>
+        <ul style={{
+            listStyle: 'none',
+            display: 'flex',
+            justifyContent: 'space-evenly'
+        }}>
+            <li> <Link to="/">Home</Link> </li>
+            <li><Link to="/my-bio">Biodata Saya</Link></li>
+        </ul>
+    </div>
+)
+    
+export default Menu
---
+++ src/pages/mybio.js
@@ -0,0 +1,14 @@
+import React from "react"
+    
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+    
+const mybioPage = () => (
+  <Layout>
+    <SEO title="About Us" />
+    <h1>About Us</h1>
+    <p>We are Web Developers.</p>
+  </Layout>
+)
+    
+export default mybioPage
